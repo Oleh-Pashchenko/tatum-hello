3aae06f3fd2fc90cbd4403663b9684c50cd19ca6
Oleh Pashchenko <oleh.pashchenko@gmail.com>
2025-03-06 10:27:13 +0100

    fixed wrong project url

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,7 @@ import './style.css';
 export function Home() {
 	return (
 		<div class="home">
-			<a href="https://preactjs.com" target="_blank">
+			<a href="https://tatum.io/" target="_blank">
 				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
 			</a>
 			<h1>Tatum Hello</h1>
