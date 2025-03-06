c33670de40085d5009ee55b0e88be71ae48cd77b
Oleh Pashchenko <oleh.pashchenko@gmail.com>
2025-03-06 10:31:06 +0100

    Refactored home index to react like component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
-import preactLogo from '../../assets/tatum.jpeg'
+import preactLogo from '../../assets/tatum.jpeg';
 import Form from './form';
 
 export function Home() {
 	return (
-		<div class="home">
-			<a href="https://tatum.io/" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
+		<div className="home">
+			<a href="https://tatum.io/" target="_blank" rel="noopener noreferrer">
+				<img src={preactLogo} alt="Preact logo" height={160} width={160} />
 			</a>
 			<h1>Tatum Hello</h1>
 			<Form />
